a8b705a76a8f0e85fbeb3ce566e6b5146988af8d
Frank Rodriguez <frankrod44@gmail.com>
2017-02-11 15:51:04 -0800

    Adding general Edge class
    To be used in future graph algorithms

---
+++ java/src/algorithms/graphtheory/Edge.java
@@ -0,0 +1,25 @@
+package algorithms.graphtheory;
+
+public class Edge implements Comparable<Edge> {
+    public final int from;
+    public final int to;
+    public final int weight;
+
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        if (weight != other.weight) {
+            return weight - other.weight;
+        }
+
+        if (from != other.from) {
+            return from - other.from;
+        }
+        return to - other.to;
+    }
+}
